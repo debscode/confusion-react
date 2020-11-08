6c830f54d2ab1b0c1420e0e831aa079decdefb19
Daniel Burbano <danielburbano19@gmail.com>
2020-11-07 21:57:33 -0500

    feat: assigment 4

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,29 +1,49 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+import { Fade, Stagger } from 'react-animation-components';
 
-
-function RenderLeader({ leader }) {
-    return (
-        <Media tag="li">
-            <Media left middle>
-                <Media object src={leader.image} alt={leader.name}></Media>
-            </Media>
-            <Media body className="ml-5">
-                <Media heading>{leader.name}</Media>
-                <p>{leader.designation}</p>
-                <p>{leader.description}</p>
-            </Media>
-        </Media>
-    );
+function RenderLeader({ leader, isLoading, errMess }) {
+    if (isLoading) {
+        return (
+            <Loading />
+        );
+    }
+    else if (errMess) {
+        return (
+            <h4>{errMess}</h4>
+        );
+    }
+    else {
+        return (
+            <Fade in>
+                <Media tag="li">
+                    <Media left middle>
+                        <Media object src={baseUrl + leader.image} alt={leader.name}></Media>
+                    </Media>
+                    <Media body className="ml-5">
+                        <Media heading>{leader.name}</Media>
+                        <p>{leader.designation}</p>
+                        <p>{leader.description}</p>
+                    </Media>
+                </Media>
+            </Fade>
+        );
+    }
 }
+
 
 function About(props) {
 
-    const leaders = props.leaders.map((leader) => {        
-        return (            
+    const leaders = props.leaders.map((leader) => {
+        return (
             <div key={leader.id}>
-                <RenderLeader leader={leader} />
+                <RenderLeader
+                    leader={leader}
+                    isLoading={props.leadersLoading}
+                    errMess={props.leadersErrMess} />
             </div>
         );
     });
@@ -84,7 +104,9 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list>
-                        {leaders}
+                        <Stagger in>
+                            {leaders}
+                        </Stagger>
                     </Media>
                 </div>
             </div>
--- src/components/ContactComponet.js
+++ src/components/ContactComponet.js
@@ -19,9 +19,8 @@ class Contact extends Component {
 
     handleSubmit(values) {
         console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
-        this.props.resetFeedbackForm();
-        // event.preventDefault();
+        this.props.postFeedback(values)        
+        this.props.resetFeedbackForm();        
     }
 
     render() {
